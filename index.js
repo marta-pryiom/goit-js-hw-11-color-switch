e39b9e56c5f949a13c98966170bdf721c86a793f
Marta Pryiom <Vynnytska1989@gmail.com>
2021-08-08 16:51:40 +0300

    1 commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const refs = {
+  startButton: document.querySelector('[data-action="start"]'),
+  stopButton: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+const timer = {
+  intervalId: null,
+  isActive: false,
+  onStartChangeColor() {
+    if (this.isActive) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+      this.isActive = true;
+      const min = 0;
+      const max = colors.length - 1;
+      refs.body.style.background = colors[randomIntegerFromInterval(min, max)];
+    }, 1000);
+  },
+  onStopChangeColor() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+refs.startButton.addEventListener(
+  'click',
+  timer.onStartChangeColor.bind(timer),
+);
+
+refs.stopButton.addEventListener('click', timer.onStopChangeColor.bind(timer));
